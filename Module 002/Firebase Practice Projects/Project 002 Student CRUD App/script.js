d4182291ffbc70c34ef26dc64f3a5b8414ecedf8
Muhammad Shoaib Khan <shoaibmuhammad960@msn.com>
2025-03-24 03:39:31 +0500

    Student Firebase Project Completed

--- Module 002/Firebase Practice Projects/Project 002 Student CRUD App/script.js
+++ Module 002/Firebase Practice Projects/Project 002 Student CRUD App/script.js
@@ -1,7 +1,7 @@
 
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
-  import { getDatabase, set,ref } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-database.js";
+  import { getDatabase, set,ref,get, remove, update } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-database.js";
 
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
@@ -24,10 +24,10 @@
   console.log("Firebase Initiated")
 
   function AddStudents(){
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const rollnumber = document.getElementById('rollnumber').value;
-    const admdate = document.getElementById('admdate').value;
+    const name = document.getElementById('namebox').value;
+    const email = document.getElementById('emailbox').value;
+    const rollnumber = document.getElementById('rollnumberbox').value;
+    const admdate = document.getElementById('admdatebox').value;
 
     console.log(name,email,rollnumber,admdate)
 
@@ -40,7 +40,95 @@
 
     notification.innerText = "Added Successfully"
 
+     document.getElementById('namebox').value = "";
+     document.getElementById('emailbox').value = "";
+     document.getElementById('rollnumberbox').value = "";
+     document.getElementById('admdatebox').value = "";
 
+    ReadData();
   }
 
   add_data.addEventListener('click',AddStudents)
+
+  // Read Data
+
+  function ReadData(){
+    const userRef = ref(db,'students/')
+
+    get(userRef).then((snapshot)=>{
+        // console.log(snapshot.val());
+        const data = snapshot.val();
+        const table = document.querySelector("table")
+        let html = '';
+
+        for (const key in data){
+            const {name,email,rollnumber,admdate} = data[key];
+            console.log(name)
+
+            html += `
+            
+            <tr>
+            <td>${name}</td>
+            <td>${email}</td>
+            <td>${rollnumber}</td>
+            <td>${admdate}</td>
+            <td class='del' onclick="deleteData('${rollnumber}')"><button>Delete</button</td>
+             <td class='up' onclick="updateData('${rollnumber}')"><button>Update</button</td>
+            </tr>
+            `
+        }
+
+        table.innerHTML =html
+    })
+  }
+
+  ReadData()
+
+
+  //Delete Data
+  window.deleteData = function(rollnumber){
+    const userRef = ref(db,`students/${rollnumber}`)
+    remove(userRef)
+    notification.innerText = "Data deleted successfully"
+    ReadData()
+  }
+
+  //Update Data
+  window.updateData = function(rollnumber){
+    const userRef = ref(db,`students/${rollnumber}`)
+    get(userRef).then((item)=>{
+      // console.log(item.val())
+      document.getElementById('namebox').value =item.val().name;
+     document.getElementById('emailbox').value = item.val().email;
+     document.getElementById('rollnumberbox').value = item.val().rollnumber;
+     document.getElementById('admdatebox').value = item.val().admdate;
+    })
+
+    document.querySelector('.update_Data').classList.add('show')
+
+    const update_btn = document.querySelector('#update_data')
+
+    update_btn.addEventListener('click',()=>{
+      const name = document.getElementById('namebox').value;
+      const email = document.getElementById('emailbox').value;
+      const rollnumber = document.getElementById('rollnumberbox').value;
+      const admdate = document.getElementById('admdatebox').value;
+
+      update(ref(db),{
+        [`students/${rollnumber}/name`] : name,
+        [`students/${rollnumber}/email`] : email,
+        [`students/${rollnumber}/rollnumber`] : rollnumber,
+        [`students/${rollnumber}/admdate`] : admdate,
+    }).then(()=>{
+              notification.innerText = "data Updated";
+              
+              document.querySelector('.update_Data').classList.remove('show')
+              document.getElementById('namebox').value="";
+              document.getElementById('emailbox').value="";
+               document.getElementById('rollnumberbox').value="";
+               document.getElementById('admdatebox').value="";
+              ReadData()
+      })
+    })
+
+  }
