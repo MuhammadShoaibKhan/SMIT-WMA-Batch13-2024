be98866d9e9fffaae25679f1ab3f8493d727c072
Muhammad Shoaib Khan <shoaibmuhammad960@msn.com>
2025-04-14 08:09:39 +0500

    Blog Updated

---
+++ Module 002/Firebase Practice Projects/Project 005 With Blog Integration/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDLtLQUpR0tXfIch6gKs_02oxlxFInqKcY",
+  authDomain: "fir-with-auth-f8de7.firebaseapp.com",
+  projectId: "fir-with-auth-f8de7",
+  storageBucket: "fir-with-auth-f8de7.firebasestorage.app",
+  messagingSenderId: "956563610489",
+  appId: "1:956563610489:web:e4652329128817626aeda8",
+  measurementId: "G-2H411VCHC4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
